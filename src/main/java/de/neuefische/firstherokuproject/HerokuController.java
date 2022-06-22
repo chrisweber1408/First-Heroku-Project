bb424f344ae2a95652ffa85dadf48c67b4457476
chris <chrisweber1991@gmail.com>
2022-06-22 11:38:56 +0200

    Test

--- src/main/java/de/neuefische/firstherokuproject/HerokuController.java
+++ src/main/java/de/neuefische/firstherokuproject/HerokuController.java
@@ -2,16 +2,23 @@ package de.neuefische.firstherokuproject;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/test")
+@RequestMapping("/api/")
 public class HerokuController {
 
+
     @GetMapping
-    public String hello(){
-        return "test";
+    public String hallo(){
+        return "Multipliziert Zahlen o.O";
+    }
+
+    @GetMapping("/{id}")
+    public int hello(@PathVariable int i){
+        return i * i * i * i;
     }
 
 }
